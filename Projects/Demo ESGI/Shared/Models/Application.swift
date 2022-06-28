b87bfd9c46efb1a1298737b4fd8891b7000ae077
Ludovic Ollagnier <ludovic@tectec.fr>
2022-06-28 19:33:25 +0200

    Add app model

---
+++ Projects/Demo ESGI/Shared/Models/Application.swift
@@ -0,0 +1,16 @@
+//
+//  Application.swift
+//  Demo ESGI
+//
+//  Created by Ludovic Ollagnier on 28/06/2022.
+//
+
+import Foundation
+
+struct Application {
+
+    let appName: String
+    let appBaseline: String
+    let appIconName: String
+    let haveInAppPurchases: Bool
+}
--- Projects/Demo ESGI/Shared/Models/FeaturedApp.swift
+++ Projects/Demo ESGI/Shared/Models/FeaturedApp.swift
@@ -5,3 +5,37 @@
 //  Created by Ludovic Ollagnier on 27/06/2022.
 //
 
+import Foundation
+
+struct FeaturedApp {
+
+    enum Category {
+        case details
+        case favorites
+    }
+
+    enum Status {
+        case notOwned
+        case owned
+        case installed
+    }
+
+    let featureImageName: String
+    let featureTitle: String
+    let featureCategory: Category
+
+    let app: Application
+
+    var appStatus: Status
+
+    mutating func buy() {
+        appStatus = Status.owned
+    }
+
+    mutating func download() {
+        appStatus = .installed
+    }
+
+    mutating func uninstall() {
+    }
+}
